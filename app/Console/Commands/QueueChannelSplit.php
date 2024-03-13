b90d792eb42c19a6f56665582a6d33ad0cb49fea
davelai <dave.lai@kkyday.com>
2024-03-13 16:18:31 +0800

    jobs demo

---
+++ app/Console/Commands/QueueChannelSplit.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\JobA;
+use App\Jobs\JobB;
+use App\Jobs\JobC;
+use Illuminate\Console\Command;
+
+class QueueChannelSplit extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'QueueChannelSplit';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        dispatch(new JobB(1))
+            ->onQueue('booking');
+
+        dispatch(new JobC(2))
+            ->onQueue('booking2');
+    }
+}
